963741b8d4850513fe198ff56a02a35631855196
EmbarkEmbrace <75050777+EmbarkEmbrace@users.noreply.github.com>
2023-06-25 01:44:47 +0000

    -

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-Age = input("What is your current age?  I'd like to determine how many days, weeks, and months you have until you turn 90. \n")
+Age = input("What is your current age? I'd like to determine how many days, weeks, and months you have until you turn 90. \n")
 Date = 90
-X = ((int(Date) - int(Age)) * int(365))
-Y = ((int(Date) - int(Age)) * int(52))
-Z = ((int(Date) - int(Age)) * int(12))
+Days = 365
+Weeks = 52
+Months = 12
+X = ((int(Date) - int(Age)) * int(Days))
+Y = ((int(Date) - int(Age)) * int(Weeks))
+Z = ((int(Date) - int(Age)) * int(Months))
 print("You have " + str(X) +  " days, " + str(Y) + " weeks, and " + str(Z) + " months left until you are " + str(Date) + " years old.")
